8da732448af49a736b2ce0352c8d7784877dbbbf
Constantine <60424812+ConstantineNam@users.noreply.github.com>
2021-03-23 00:33:56 -0400

    Updated LinkedIn Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer>
             <h4>Find me on</h4>
             <div className='soc-media'>
-                <a href='https://www.linkedin.com/in/constantine-nam/' target='_blank' rel='noreferrer'><LinkedInIcon/></a>
+                <a href='https://www.linkedin.com/in/c-nam/' target='_blank' rel='noreferrer'><LinkedInIcon/></a>
                 <a href='https://github.com/ConstantineNam' target='_blank' rel='noreferrer'><GithubIcon/></a>  
             </div>
             <p>{(new Date().getFullYear())}</p>
